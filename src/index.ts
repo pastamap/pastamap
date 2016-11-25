412456a80a551f795f862116c109790ab5a5c0f6
danzel <dave@smartrak.co.nz>
2016-11-25 16:45:12 +1300

    Add a map

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,25 @@
 import './index.css';
 
-console.log('test');
+import * as L from 'leaflet';
+
+document.getElementById('load-data').onclick = () => {
+	alert('lets go');
+}
+
+let map = L.map('map', {
+    center: L.latLng(0, 0),
+	zoom: 0,
+
+	layers:
+	[
+		L.tileLayer('http://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}@2x.png?access_token={accessToken}', {
+			attribution: 'Imagery from <a href="http://mapbox.com/about/maps/">MapBox</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+			subdomains: 'abcd',
+			id: 'mapbox.streets',
+			accessToken: 'pk.eyJ1IjoiZGF2ZWxlYXZlciIsImEiOiJjaXZ4OGtxbGcwMWR6MnlvMnZzY3ljejMxIn0.4aRVH6XUayzkw6c4XtCkuw'
+		})
+		/*L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+		attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+	})*/
+	],
+});
