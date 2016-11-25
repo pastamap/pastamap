5222be6f6898f4371e18cf144639a2e0a40dd9c2
danzel <dave@smartrak.co.nz>
2016-11-25 14:06:16 +1300

    Basic webpack+typescript+css page with reload on change.

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var path = require('path');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	context: __dirname + path.sep + 'src',
+	entry: './index.ts',
+	devtool: 'inline-source-map',
+	output: {
+		path: __dirname + path.sep + 'dist',
+		filename: 'bundle.js'
+	},
+    resolve: {
+        extensions: ['', '.webpack.js', '.web.js', '.ts', '.js']
+    },
+	module: {
+		loaders: [
+			{ test: /\.tsx?$/, loader: 'ts-loader' },
+			{ test: /\.css$/, loader: ExtractTextPlugin.extract("css-loader") },
+		]
+	},
+	plugins: [
+		new ExtractTextPlugin("bundle.css"),
+		new HtmlWebpackPlugin({
+			filename: 'index.html',
+			template: './index.template.html',
+			hash: true,
+			inject: true
+		})
+	]
+};
