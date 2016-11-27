cce78b3e93f973c7ad74d011b86df058a35eabe0
danzel <dave@smartrak.co.nz>
2016-11-28 10:12:25 +1300

    Hopefully build with webpack, bail on build fail?

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,16 @@ var path = require('path');
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
+const plugins = [];
+const TRAVIS = process.env.TRAVIS ? JSON.parse(process.env.TRAVIS) : false;
+if (TRAVIS) {
+	console.log('TRAVIS mode (will fail on error)');
+	plugins.push(new webpack.NoErrorsPlugin());
+}
+
+
 module.exports = {
+	bail: TRAVIS,
 	context: __dirname + path.sep + 'src',
 	entry: './index.ts',
 	devtool: 'inline-source-map',
@@ -11,9 +20,9 @@ module.exports = {
 		path: __dirname + path.sep + 'dist',
 		filename: 'bundle.js'
 	},
-    resolve: {
-        extensions: ['', '.webpack.js', '.web.js', '.ts', '.js']
-    },
+	resolve: {
+		extensions: ['', '.webpack.js', '.web.js', '.ts', '.js']
+	},
 	module: {
 		loaders: [
 			{ test: /\.tsx?$/, loader: 'ts-loader' },
